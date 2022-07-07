0916d6f7f45b5c2c0fa7623bf4f6216f8f674b15
Mike Lee <mike.lee816@gmail.com>
2022-07-07 16:17:33 -0400

    Make Nav responsive with media queries. Use responsive Nav instead of react-responsive in PageLayout.

--- components/nav.component.tsx
+++ components/nav.component.tsx
@@ -3,31 +3,37 @@ import Link from 'next/link';
 import styles from '../styles/Nav.module.scss';
 
 import CloseIcon from './close-icon.component';
+import MenuIcon from './menu-icon.component';
 
 interface Props {
+    navVisible: boolean,
     setNavVisible: React.Dispatch<React.SetStateAction<boolean>>
 }
 
-const Nav: React.FC<Props> = ({ setNavVisible }) => (
-    <nav className={styles['nav']} data-testid='nav'>
-        <button className={styles['close-button']} onClick={() => setNavVisible(false)}>
-            <CloseIcon size={30} />
+const Nav: React.FC<Props> = ({ navVisible, setNavVisible }) => (
+    <div className={styles['navigation']}>
+        <button className={`${styles['nav-toggle']} ${navVisible ? `${styles['invisible']}` : ''}`} onClick={() => setNavVisible(true)}>
+            <MenuIcon size={60} />
         </button>
-        <ul className={styles['nav-list']}>
-            <li>
-                <Link href='/'><a className={styles['nav-link']}>Home</a></Link>
-            </li>
-            <li>
-                <Link href='/projects'><a className={styles['nav-link']}>Projects</a></Link>
-            </li>
-            <li>
-                <Link href='https://github.com/mikelee'><a className={styles['nav-link']}>Github</a></Link>
-            </li>
-            <li>
-                <Link href='/contact'><a className={styles['nav-link']}>Contact</a></Link>
-            </li>
-        </ul>
-    </nav>
+        <nav className={`${styles['nav']} ${navVisible ? `${styles['visible']}` : ''}`} data-testid='nav'>
+            <button className={styles['close-button']} onClick={() => setNavVisible(false)}>
+                <CloseIcon size={30} />
+            </button>
+            <ul className={styles['nav-list']}>
+                <li>
+                    <Link href='/'><a className={styles['nav-link']}>Home</a></Link>
+                </li>
+                <li>
+                    <Link href='/projects'><a className={styles['nav-link']}>Projects</a></Link>
+                </li>
+                <li>
+                    <Link href='https://github.com/mikelee'><a className={styles['nav-link']}>Github</a></Link>
+                </li>
+                <li>
+                    <Link href='/contact'><a className={styles['nav-link']}>Contact</a></Link>
+                </li>
+            </ul>
+        </nav>
+    </div>
 );
-
 export default Nav;
--- components/page-layout.component.tsx
+++ components/page-layout.component.tsx
@@ -1,7 +1,5 @@
 import { useEffect, useState } from 'react';
-import { useMediaQuery } from 'react-responsive';
 
-import MenuBar from './menu-bar.component';
 import Nav from './nav.component';
 
 interface Props {
@@ -9,37 +7,18 @@ interface Props {
 }
 
 const PageLayout: React.FC<Props> = ({ children }) => {
-
     const [navVisible, setNavVisible] = useState(false);
-
-    const isSmall = useMediaQuery({ query: '(max-width: 720px)' });
     
     useEffect(() => {
         setNavVisible(false);
     }, [children]);
 
-    if (isSmall) {
-        return (
-            <>
-                {
-                    navVisible
-                    ? <Nav setNavVisible={setNavVisible} />
-                    :
-                        <>
-                            <MenuBar setNavVisible={setNavVisible} />
-                            {children}
-                        </>
-                }
-            </>
-        )
-    } else {
-        return (
-            <>
-                <Nav setNavVisible={setNavVisible} />
-                {children}
-            </>
-        )
-    }
+    return (
+        <>
+            <Nav navVisible={navVisible} setNavVisible={setNavVisible} />
+            {children}
+        </>
+    );
 }
 
 export default PageLayout;
