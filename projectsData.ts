c69609bd4a4d734b18d59b010cabe3439af88d12
Mike Lee <mike.lee816@gmail.com>
2022-11-19 21:43:33 -0500

    Add project videos

--- projectsData.ts
+++ projectsData.ts
@@ -3,6 +3,7 @@ interface Project {
     name: string,
     description: string,
     image: string,
+    video: string,
     url: string
 }
 
@@ -12,6 +13,7 @@ const projects: Project[] = [
         name: 'Gravity Simulator',
         description: 'Create planets and watch how their gravitational forces influence each other\'s orbits.',
         image: '/gravity-simulator-image.png',
+        video: '/gravity-simulator-video.mp4',
         url: 'https://gravitysimulator.herokuapp.com/'
     },
     {
@@ -19,6 +21,7 @@ const projects: Project[] = [
         name: 'Daily Achievements',
         description: 'A to-do list app with a twist. To-dos are linked to rewards. When all the required to-dos are completed, the reward is unlocked.',
         image: '/daily-achievements-image.png',
+        video: '/daily-achievements-video.mp4',
         url: 'https://dailyachievements.herokuapp.com/'
     },
     {
@@ -26,6 +29,7 @@ const projects: Project[] = [
         name: 'Mind Right',
         description: 'Keep a postive mindset and stay focused on what matters. Add quotes with a background image. Simply click shuffle when you need a reminder and one of your quotes will appear.',
         image: '/mind-right-image.png',
+        video: '/mind-right-video.mp4',
         url: 'https://mind-right-react.herokuapp.com/'
     },
     {
@@ -33,6 +37,7 @@ const projects: Project[] = [
         name: 'Sample business',
         description: 'Take a look at what your business\'s presence on the web could look like. Complete with SEO.',
         image: '/sample-business-image.png',
+        video: '/gravity-simulator-video.mp4',
         url: 'https://sample-business.vercel.app/'
     }
 ];
