83b5670c5cd35c2ecca25b055880333911aa4a90
Mike Lee <mike.lee816@gmail.com>
2022-12-02 23:55:53 -0500

    Update Gravity Simulator and Mind Right URLs

--- projectsData.ts
+++ projectsData.ts
@@ -14,7 +14,7 @@ const projects: Project[] = [
         description: 'Create planets and watch how their gravitational forces influence each other\'s orbits.',
         image: '/gravity-simulator-image.png',
         video: '/gravity-simulator-video.mp4',
-        url: 'https://gravitysimulator.herokuapp.com/'
+        url: 'https://gravitysimulator.onrender.com/'
     },
     {
         id: 2,
@@ -30,7 +30,7 @@ const projects: Project[] = [
         description: 'Keep a postive mindset and stay focused on what matters. Add quotes with a background image. Simply click shuffle when you need a reminder and one of your quotes will appear.',
         image: '/mind-right-image.png',
         video: '/mind-right-video.mp4',
-        url: 'https://mind-right-react.herokuapp.com/'
+        url: 'https://mindright.onrender.com/'
     },
     {
         id: 4,
