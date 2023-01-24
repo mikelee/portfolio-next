c4a1e7e7c4b795232b26bd5b2a20950883ec04fa
Mike Lee <mike.lee816@gmail.com>
2023-01-23 21:48:39 -0500

    Remove unneeded recaptcha script
    Recaptcha script is loaded on contact page instead.

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,12 +1,10 @@
 import { Html, Head, Main, NextScript } from 'next/document';
-import Script from 'next/script';
 
 export default function Document() {
     return (
         <Html lang='en'>
             <Head />
             <body>
-                <Script src="https://www.google.com/recaptcha/api.js" async defer></Script>
                 <Main />
                 <NextScript />
             </body>
