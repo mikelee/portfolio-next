489309bf7eb01ead183348fc7ea8c5dc5d6c23c6
Mike Lee <mike.lee816@gmail.com>
2023-06-15 14:35:36 -0400

    Remove trailingSlash

--- next.config.js
+++ next.config.js
@@ -3,8 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
   sassOptions: {
     additionalData: '@import "styles/variables.scss";'
-  },
-  trailingSlash: true
+  }
 }
 
 module.exports = nextConfig
