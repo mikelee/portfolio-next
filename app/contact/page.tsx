11d8ec729a1fe3b15fb2e5fcbdfb4f10a751bec6
Mike Lee <mike.lee816@gmail.com>
2024-05-23 19:58:06 -0400

    Move contact form into it's own client component so Contact page doesn't have to be a client component

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,68 +1,24 @@
-'use client'
-
 import Head from 'next/head';
 import Script from 'next/script';
-import React, { useEffect, useRef } from 'react';
+import React from 'react';
 
 import styles from '../../styles/Contact.module.scss';
 
-declare global {
-    interface Window {
-        onSubmit: (token: string) => Promise<void>
-    }
-}
+import ContactForm from '../../components/contact-form';
 
-const Contact = () => {
-
-    const formRef = useRef<HTMLFormElement>(null);
-
-    useEffect(() => {
-        (window).onSubmit = async function(token: string) {
-            const requiredFieldsValid = areRequiredFieldsValid();
-
-            if (requiredFieldsValid) {
-                formRef.current?.submit();
-            } else {
-                (window as any).grecaptcha.reset();
-            }
-        }
-    }, [formRef]);
-
-    const areRequiredFieldsValid = () => {
-        const requiredElements = document.querySelectorAll("[required]");
-
-        for (let i = 0; i < requiredElements.length; i++) {
-            // if not valid
-            if (!(requiredElements[i] as HTMLFormElement).reportValidity()) {
-                return false;
-            }
-        }
-
-        return true;
-    }
-
-    return (
-        <>
-            <Head>
-                <title>Mike Lee | Contact</title>
-                <meta name='description' content='Send a message to contact Mike Lee.' />
-            </Head>
-            <Script src='https://www.google.com/recaptcha/api.js' />
-            <main className={styles['contact']}>
-                <h1>Contact</h1>
-                <p>Let&apos;s get in touch!</p>
-                <form ref={formRef} action={'https://usebasin.com/f/e94fc554851b'} method='POST' >
-                    <label htmlFor='name'>Name</label>
-                    <input type='text' name='name' placeholder='Your name' required autoFocus />
-                    <label htmlFor='email'>Email</label>
-                    <input type='email' name='email' placeholder='username@example.com' required />
-                    <label htmlFor='message'>Message</label>
-                    <textarea name='message' rows={12} placeholder="What's on your mind..." required />
-                    <button className='g-recaptcha' data-sitekey='6Lew3SMUAAAAAJ82QoS7gqOTkRI_dhYrFy1f7Sqy' data-callback='onSubmit' data-action='submit'>Submit</button>
-                </form>
-            </main>
-        </>
-    );
-}
+const Contact = () => (
+    <>
+        <Head>
+            <title>Mike Lee | Contact</title>
+            <meta name='description' content='Send a message to contact Mike Lee.' />
+        </Head>
+        <Script src='https://www.google.com/recaptcha/api.js' />
+        <main className={styles['contact']}>
+            <h1>Contact</h1>
+            <p>Let&apos;s get in touch!</p>
+            <ContactForm />
+        </main>
+    </>
+);
 
 export default Contact;
