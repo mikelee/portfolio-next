e817413b9bc41daccc2c1b3e3cd9b0c868c771a6
Mike Lee <mike.lee816@gmail.com>
2024-06-14 15:18:46 -0400

    Add clip-path so there is an angle at the bottom edge

--- sections/landing/landing.tsx
+++ sections/landing/landing.tsx
@@ -4,16 +4,18 @@ import styles from './landing.module.scss';
 import Mountains from '../../components/mountains/mountains.component';
 
 const Landing = () => (
-    <div className={styles['landing']}>
-        <main>
-            <h1 className={styles['name']}>Mike Lee</h1>
-            <p className={styles['text']}>Full Stack Developer</p>
-            <div className={styles['buttons']}>
-                <Link href='/projects' className={styles['button']}>Projects</Link>
-                <Link href='/contact' className={styles['button']}>Get In Touch</Link>
-            </div>
-        </main>
-        <Mountains />
+    <div className={styles['landing-wrapper']}>
+        <div className={styles['landing']}>
+            <main>
+                <h1 className={styles['name']}>Mike Lee</h1>
+                <p className={styles['text']}>Full Stack Developer</p>
+                <div className={styles['buttons']}>
+                    <Link href='/projects' className={styles['button']}>Projects</Link>
+                    <Link href='/contact' className={styles['button']}>Get In Touch</Link>
+                </div>
+            </main>
+            <Mountains />
+        </div>
     </div>
 );
 
