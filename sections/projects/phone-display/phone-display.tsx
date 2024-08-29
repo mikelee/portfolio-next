7ac346f894b4d5259b8bfbfbda16ba1d12e4365a
Mike Lee <mike.lee816@gmail.com>
2024-08-29 12:26:47 -0400

    Move code that displays the projects into its own component called PhoneDisplay. Use PhoneDisplay component.
    This component is called PhoneDisplay because it displays all of the projects using the Phone svg. Later, there will be another component that displays the projects using the Laptop svg.

---
+++ sections/projects/phone-display/phone-display.tsx
@@ -0,0 +1,31 @@
+import Link from 'next/link';
+
+import styles from './phone-display.module.scss';
+
+import { Project } from '../../../projectsData';
+
+import Phone from '../../../components/phone/phone';
+
+interface Props {
+    projects: Project[]
+}
+
+const PhoneDisplay: React.FC<Props> = ({ projects }) => (
+    <div className={styles['phone-display']}>
+        {
+            projects.map(project => (
+                <section key={project.id} className={styles['project']}>
+                    <div className={styles['name-container']}>
+                        <h2 className={styles['name']}>{project.name}</h2>
+                    </div>
+                    <div className={styles['phone-container']}>
+                        <Phone imageURL={project.image} />
+                    </div>
+                    <Link href={project.url} className={styles['try-project-button']}>Try it out!</Link>
+                </section>
+            ))
+        }
+    </div>
+);
+
+export default PhoneDisplay;
--- sections/projects/projects.tsx
+++ sections/projects/projects.tsx
@@ -1,28 +1,13 @@
-import Link from 'next/link';
 import projectsData from '../../projectsData';
 
 import styles from './projects.module.scss';
 
-import Phone from '../../components/phone/phone';
+import PhoneDisplay from './phone-display/phone-display';
 
 const Projects = () => (
     <div id='projects' className={styles['projects']}>
         <h1 className={styles['title']}>Projects</h1>
-        <div className={styles['projects-display']}>
-            {
-                projectsData.map(project => (
-                    <section key={project.id} className={styles['project']}>
-                        <div className={styles['name-container']}>
-                            <h2 className={styles['name']}>{project.name}</h2>
-                        </div>
-                        <div className={styles['phone-container']}>
-                            <Phone imageURL={project.image} />
-                        </div>
-                        <Link href={project.url} className={styles['try-project-button']}>Try it out!</Link>
-                    </section>
-                ))
-            }
-        </div>
+        <PhoneDisplay projects={projectsData} />
     </div>
 );
 
