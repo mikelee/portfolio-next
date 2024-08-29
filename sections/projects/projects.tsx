a814ecec01dba75b69e15353928464876965e101
Mike Lee <mike.lee816@gmail.com>
2024-08-29 12:33:30 -0400

    Render PhoneDisplay on small devices and LaptopDisplay on large devices (> 720px)

--- sections/projects/projects.tsx
+++ sections/projects/projects.tsx
@@ -2,13 +2,26 @@ import projectsData from '../../projectsData';
 
 import styles from './projects.module.scss';
 
+import useWindowSize from '../../hooks/useWindowSize';
+
+import LaptopDisplay from './laptop-display/laptop-display';
 import PhoneDisplay from './phone-display/phone-display';
 
-const Projects = () => (
-    <div id='projects' className={styles['projects']}>
-        <h1 className={styles['title']}>Projects</h1>
-        <PhoneDisplay projects={projectsData} />
-    </div>
-);
+const Projects = () => {
+    const SMALL_DEVICE_WIDTH = 720;
+
+    const { width: screenWidth } = useWindowSize();
+    
+    return (
+        <div id='projects' className={styles['projects']}>
+            <h1 className={styles['title']}>Projects</h1>
+            {
+                screenWidth <= SMALL_DEVICE_WIDTH
+                ? <PhoneDisplay projects={projectsData} />
+                : <LaptopDisplay projects={projectsData} />
+            }
+        </div>
+    );
+}
 
 export default Projects;
