d6feb688f22bf4a46f3b3436e2c067bc52cdc86f
Mike Lee <mike.lee816@gmail.com>
2024-08-29 12:27:39 -0400

    Create LaptopDisplay component for Projects section

---
+++ sections/projects/laptop-display/laptop-display.tsx
@@ -0,0 +1,32 @@
+import { useState } from 'react';
+
+import styles from './laptop-display.module.scss';
+
+import { Project } from '../../../projectsData';
+
+import Laptop from '../../../components/laptop/laptop';
+
+interface Props {
+    projects: Project[]
+}
+
+const LaptopDisplay: React.FC<Props> = ({ projects }) => {
+    const [currentProject, setCurrentProject] = useState(projects[0]);
+
+    return (
+        <section className={styles['laptop-display']}>
+            <div className={styles['project-selector']}>
+                {
+                    projects.map(project => (
+                        <div key={project.id} className={`${styles['project']} ${currentProject.id === project.id ? styles['selected'] : ''}`} onMouseEnter={() => setCurrentProject(project)}>{project.name}</div>
+                    ))
+                }
+            </div>
+            <div className={styles['laptop-container']}>
+                <Laptop imageURL={currentProject.image} />
+            </div>
+        </section>
+    );
+}
+
+export default LaptopDisplay;
