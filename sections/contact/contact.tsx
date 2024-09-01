b50590d81af19313323220492e9c004824660020
Mike Lee <mike.lee816@gmail.com>
2024-09-01 14:22:42 -0400

    Use NetlifyForm instead of ContactForm
    Changes:
    - reCAPTCHA script was removed because it is not used with NetlifyForm
    - .form-container wraps NetlifyForm to set NetlifyForm's width

--- sections/contact/contact.tsx
+++ sections/contact/contact.tsx
@@ -1,19 +1,35 @@
-import Script from 'next/script';
 import React from 'react';
 
 import styles from './contact.module.scss';
 
-import ContactForm from '../../components/contact-form/contact-form';
+import NetlifyForm from '../../components/netlify-form/netlify-form';
 
 const Contact = () => (
-    <>
-        <Script src='https://www.google.com/recaptcha/api.js' />
-        <div id='contact' className={styles['contact']}>
-            <h1>Contact</h1>
-            <p>Let&apos;s get in touch!</p>
-            <ContactForm />
+    <div id='contact' className={styles['contact']}>
+        <h1>Contact</h1>
+        <p>Let&apos;s get in touch!</p>
+        <div className={styles['form-container']}>
+            <NetlifyForm
+                name='contact'
+                successMessage='Thank you for your message!'
+                errorMessage='Oh no! Something went wrong.'
+            >
+                <label>
+                    Name
+                    <input type='text' name='name' placeholder='Your name' required />
+                </label>
+                <label>
+                    Email
+                    <input type='email' name='email' placeholder='username@example.com' required />
+                </label>
+                <label>
+                    Message
+                    <textarea name='message' rows={12} placeholder="What's on your mind..." required />
+                </label>
+                <button type='submit'>Submit</button>
+            </NetlifyForm>
         </div>
-    </>
+    </div>
 );
 
 export default Contact;
